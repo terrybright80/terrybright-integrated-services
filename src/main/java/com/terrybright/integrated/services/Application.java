944d028de524b4046a9d99cd9f72e255710b10d5
bright <brightliontech@gmail.com>
2025-11-03 22:23:00 +0100

    updated files

--- src/main/java/com/terrybright/integrated/services/Application.java
+++ src/main/java/com/terrybright/integrated/services/Application.java
@@ -1,3 +1,5 @@
+package com.terrybright.integrated.services;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/terrybright/integrated/services/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.terrybright.integrated.services.repository;
-
-import com.terrybright.integrated.services.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
