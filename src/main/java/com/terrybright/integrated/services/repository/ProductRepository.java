0c97fe2873ee9ef3d4b5a99bd2af575a2aefbe5f
bright <brightliontech@gmail.com>
2025-11-03 12:55:30 +0100

    updated productService.java

--- src/main/java/com/terrybright/integrated/services/repository/ProductRepository.java
+++ src/main/java/com/terrybright/integrated/services/repository/ProductRepository.java
@@ -1,4 +1,3 @@
-// ...existing code...
 package com.terrybright.integrated.services.service;
 
 import com.terrybright.integrated.services.model.Product;
@@ -11,7 +10,9 @@ public interface ProductService {
     Product create(Product product);
     Product update(Long id, Product product);
     void delete(Long id);
-}package com.terrybright.integrated.services.repository;
+}
+
+package com.terrybright.integrated.services.repository;
 
 import com.terrybright.integrated.services.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
